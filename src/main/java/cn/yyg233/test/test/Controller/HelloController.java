75dcbc7232bfcb2555b67aed8f8b9c1ea14827df
yige.yyg <yige.yyg@alibaba-inc.com>
2019-07-01 22:42:47 +0800

    first commit

---
+++ src/main/java/cn/yyg233/test/test/Controller/HelloController.java
@@ -0,0 +1,20 @@
+package cn.yyg233.test.test.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author yige.yyg
+ * @date 2019-07-01 10:28 PM
+ **/
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
---
+++ src/test/java/cn/yyg233/test/test/TestApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.yyg233.test.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
